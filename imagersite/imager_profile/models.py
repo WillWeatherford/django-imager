53ff13fa0822dbabf554360bf000f8b3bfe41f40
Will Weatherford <weatherford.william@gmail.com>
2016-04-08 16:52:05 -0700

    Set up basic imagerprofile and made first migration

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,13 @@
+"""Establish models for the imager site's User Profile."""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ImagerProfile(models.Model):
+    """Profile attached to User models by one-to-one relationship."""
+
+    user = models.OneToOneField(User, related_name='profile')
+    is_active = models.BooleanField(default=False)
+    location = models.CharField(default='')
+    camera = models.CharField(default='')
+    fav_photo = models.CharField(default='')
