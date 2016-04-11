ed8217e29830f5e1e29bae3334e762dc44b0225f
Will Weatherford <weatherford.william@gmail.com>
2016-04-10 21:30:26 -0700

    Introduced DeletedUserCase

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -32,19 +32,29 @@ class OneUserCase(TestCase):
         self.user.set_password('secret')
 
 
-class BasicUserProfileCase(OneUserCase):
-    """Simple test case for Photos."""
+class DeletedUserCase(OneUserCase):
+    """Inheritable base case setting up one User, then deleting it."""
 
     def setUp(self):
         """Set up User models for testing."""
-        super(BasicUserProfileCase, self).setUp()
+        super(DeletedUserCase, self).setUp()
+        self.user.delete()
 
-        self.deleted_user = UserFactory.create(
-            username='deleteme',
-            email='deleteme@example.com',
-        )
-        self.deleted_user.set_password('notsecret')
-        self.deleted_user.delete()
+    def test_no_profile_pk_after_delete(self):
+        """Test that profile is deleted with User, losing its primary key."""
+        self.assertIsNone(self.user.profile.pk)
+
+    def test_profile_is_active_false(self):
+        """Test that profile of deleted User is not active."""
+        self.assertFalse(self.user.profile.is_active)
+
+    def test_active_count(self):
+        """Test that counting the active manager returns expected int."""
+        self.assertFalse(ImagerProfile.active.count())
+
+
+class BasicUserProfileCase(OneUserCase):
+    """Simple test case for Photos."""
 
     def test_user_has_profile(self):
         """Test that newly created User does have a profile attached."""
@@ -55,17 +65,9 @@ class BasicUserProfileCase(OneUserCase):
         self.assertIsInstance(self.user.profile.pk, int)
         self.assertTrue(self.user.profile.pk)
 
-    def test_no_profile_pk_after_delete(self):
-        """Test that profile is deleted with User, losing its primary key."""
-        self.assertIsNone(self.deleted_user.profile.pk)
-
     def test_profile_is_active(self):
         """Test that profile of new User is active."""
         self.assertTrue(self.user.profile.is_active)
-
-    def test_profile_is_active_false(self):
-        """Test that profile of deleted User is not active."""
-        self.assertFalse(self.deleted_user.profile.is_active)
 
     def test_profile_is_active_false_2(self):
         """Test that profile of deactivated User is not active."""
