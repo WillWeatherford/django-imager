b73822d186e3a498fdf2d92874e7487d1372b5ec
Will Weatherford <weatherford.william@gmail.com>
2016-04-12 18:44:32 -0700

    Working stable homepage view and url.

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from .views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view(), name='home_page'),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
 ]
