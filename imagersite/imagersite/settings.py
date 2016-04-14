e949a59f3ef6b1b89c22073a33b09af2ac1cc303
Will Weatherford <weatherford.william@gmail.com>
2016-04-13 17:40:47 -0700

    Successfully loading registration and login pages. Posting gets a 500 error.

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import sys
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+ACCOUNT_ACTIVATION_DAYS = 7
