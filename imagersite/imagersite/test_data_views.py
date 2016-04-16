fc9aaa37f767f6c63fb9a22097e1d10273fe3b72
Will Weatherford <weatherford.william@gmail.com>
2016-04-16 15:33:39 -0700

    SetUp creates separate client sessions per user.

--- imagersite/imagersite/test_data_views.py
+++ imagersite/imagersite/test_data_views.py
@@ -1,7 +1,7 @@
 """Tests for profile, library, album and photo views."""
 from __future__ import unicode_literals
 from django.test import Client, TestCase
-from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth.models import User
 from imager_profile.tests import UserFactory
 from .test_auth import user_from_response
 
@@ -10,7 +10,7 @@ HOME = '/'
 REG = '/accounts/register/'
 LOGIN = '/accounts/login/'
 LOGOUT = '/accounts/logout/'
-PROFILE = '/accounts/logout/'
+PROFILE = '/accounts/profile/'
 LIBRARY = '/images/library/'
 ALBUM = '/images/album/'
 PHOTO = '/images/photo/'
@@ -32,13 +32,15 @@ class AuthenticatedCase(TestCase):
 
     def setUp(self):
         """Set up for unauthenticated case with no users."""
-        self.client = Client()
         self.user_batch = UserFactory.create_batch(NUM_USERS)
+        self.users_sessions = []
 
         for user in self.user_batch:
+            client = Client()
             params = {'username': user.username, 'password': 'secret'}
-            response = self.client.post(LOGIN, params, follow=True)
-            setattr(self, '{}_response'.format(user.username), response)
+            response = client.post(LOGIN, params, follow=True)
+            session = {'user': user, 'client': client, 'login_response': response}
+            self.users_sessions.append(session)
 
     def test_num_users(self):
         """Test there are as many users in the database as we registered."""
@@ -46,14 +48,35 @@ class AuthenticatedCase(TestCase):
 
     def test_all_logged_in(self):
         """Test that all users are logged in."""
-        for user in self.user_batch:
-            response = getattr(self, '{}_response'.format(user.username))
-            context_user = user_from_response(response)
+        for session in self.users_sessions:
+            user = session['user']
+            context_user = user_from_response(session['login_response'])
             self.assertEqual(user, context_user)
             self.assertTrue(user.is_authenticated())
             self.assertTrue(context_user.is_authenticated())
 
-    # test that profile page is loaded from each response
+    def test_login_to_profile(self):
+        """Test that response has redirected to profile page."""
+        for session in self.users_sessions:
+            login_response = session['login_response']
+            self.assertEqual(login_response.status_code, 200)
+            self.assertEqual(login_response.request['PATH_INFO'], PROFILE)
+
+    def test_library_ok(self):
+        """Test that library page is accessible."""
+        for session in self.users_sessions:
+            client = session['client']
+            response = client.get('/images/library/')
+            self.assertEqual(response.status_code, 200)
+
+    def test_library_ok(self):
+        """Test that library page is accessible."""
+        for session in self.users_sessions:
+            client = session['client']
+            response = client.get('/images/library/')
+            self.assertEqual(response.status_code, 200)
+
+
     # test that library page is accessible
     # test default image if no cover
     # test that owners see all of their albums
