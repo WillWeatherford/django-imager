f09d15f25b21e6a9b54d5d057f10aabbf85381d7
Will Weatherford <weatherford.william@gmail.com>
2016-04-21 17:30:31 -0700

    Updated settings for production deployment.

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(vk)!5j=k18q1eo$2^c=18x0h)b14ijz1q3$^f9#i(awy4g(o('
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-52-39-88-2.us-west-2.compute.amazonaws.com']
 
 
 # Application definition
@@ -132,8 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
