956611dbcb29e125c98fdb0d68b64054282d271f
Will Weatherford <weatherford.william@gmail.com>
2016-04-24 20:09:07 -0700

    Removed commented-out old code in imager_images.models.

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -55,10 +55,6 @@ class Photo(md.Model):
         return "{}(title={}, owner={}, date_published={}".format(
             name, self.title[:20], self.owner, _pub_date(self))
 
-    # def get_url(self):
-    #     """Return string of url for single view."""
-    #     return '/images/photo/{}/'.format(self.pk)
-
 
 @python_2_unicode_compatible
 class Album(md.Model):
@@ -119,12 +115,6 @@ class Album(md.Model):
         for photo in self._owned_photos(photos):
             photo.albums.add(self)
             photo.save()
-            # if self.cover is None:
-            #     self.set_cover(photo)
-
-    # def get_url(self):
-    #     """Return string of url to get to single album view."""
-    #     return '/images/album/{}/'.format(self.pk)
 
     def get_cover(self):
         """Return the user set cover of album or a default image."""
